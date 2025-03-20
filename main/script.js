40e157c613dae692709f646b64a8434ad71f9a3a
mBadar-dev <m.badar.dev@gmail.com>
2025-03-20 11:20:32 +0500

    update  password generation

---
+++ main/script.js
@@ -0,0 +1,20 @@
+const passwordBox = document.getElementById("input");
+const lenght = 12;
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "!@#$%^&*()_+~|/[]{}?<=>-";
+const allChars = upperCase + lowerCase + number + symbol;
+
+function createPassword() {
+  let password = "";
+  while (password.length < lenght) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+  passwordBox.value = password;
+}
+
+function copyPassword() {
+  passwordBox.select();
+  document.execCommand("copy");
+}
